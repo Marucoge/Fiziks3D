4958d98d79f1bffb3492c478b2a66f6b60401a57
qonfi <confidentialrendezvous@gmail.com>
2017-06-05 20:01:28 +0900

    Add files via upload

---
+++ Unattachables/GravityCalculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+namespace Fizix
+{
+
+
+    /// <summary>
+    /// コンポーネントではない。
+    /// </summary>
+    public class GravityCalculator : IGravityCalculator
+    {
+
+        public float GravityAccel { get; set; }
+
+
+        public GravityCalculator()
+        {
+            GravityAccel = 30f;
+        }
+
+
+        public float CalcMovement(float floatingTime)
+        {
+            return floatingTime * GravityAccel;
+        }
+
+    }
+
+
+}
